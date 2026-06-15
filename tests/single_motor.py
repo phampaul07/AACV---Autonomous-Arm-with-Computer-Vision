db3a3c4350ae649f039dfb56a44bff93390409c7
Paul Pham <phampp07@gmail.com>
2026-06-15 13:36:01 -0500

    first update

--- tests/single_motor.py
+++ tests/single_motor.py
@@ -1,1 +1,10 @@
+from lewansoul_servo_bus import ServoBus
 
+servo_bus = ServoBus('/dev/ttyUSB0')
+
+# Move servo with ID 1 to 90 degrees in 1.0 seconds
+servo_bus.move_time_write(1, 90, 1.0)
+
+# Move servo with ID 2 to 180 degrees in 2.0 seconds
+servo_2 = servo_bus.get_servo(2)
+servo_2.move_time_write(180, 2.0)
